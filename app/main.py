86a67973cf63a1200ea329d9d52ff90043d66fb2
DarshanS-Dev <darshans.sys@gmail.com>
2026-05-11 10:20:39 +0000

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import projects, evals
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(projects.router)
 app.include_router(evals.router)
